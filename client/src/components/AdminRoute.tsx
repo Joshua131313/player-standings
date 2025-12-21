45aceebb6c31a7500b4d7bc9f326a7f11c7749af
Joshua Bitton <joshtosh13@hotmail.com>
2025-12-21 12:47:17 -0500

    c

--- client/src/components/AdminRoute.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { isAdmin } from "@/auth/adminAuth";
-
-export const AdminRoute = ({ children }: { children: JSX.Element }) => {
-  return isAdmin() ? children : <Navigate to="/admin-login" replace />;
-};
