2c01cdcd5e8cefc3ea2d87c7313b08eef54d0c90
Joshua Bitton <joshtosh13@hotmail.com>
2025-12-21 12:59:50 -0500

    Revert "Reverted breaking changes"
    This reverts commit 397da5fcb36b5491af5b45945468b07ffc9a8caa.

--- server/dev.js
+++ server/dev.js
@@ -1,1 +1,6 @@
-import "./app.js";
+// server/dev.js
+import "dotenv/config";
+import app from "./app.js";
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`✅ API running on http://localhost:${port}`));
